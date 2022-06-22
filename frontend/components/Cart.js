6b7a67c2369ed09c902c41109f098013f5fd85a1
Clifford Morin <cemorin21@gmail.com>
2022-06-22 02:32:25 -0400

    created the cart, added some styling to the cart

---
+++ frontend/components/Cart.js
@@ -0,0 +1,52 @@
+import { useStateContext } from "../lib/context.js";
+import {
+  CartWrapper,
+  CartStyle,
+  Card,
+  CardInfo,
+  EmptyStyle,
+  Checkout,
+} from "../styles/CartStyles.js";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+
+export default function Cart() {
+  const { cartItems } = useStateContext();
+
+  return (
+    <CartWrapper>
+      <CartStyle>
+        {cartItems.length < 1 && (
+          <EmptyStyle>
+            <h1>You have more shopping to do 😎</h1>
+            <FaShoppingCart />
+          </EmptyStyle>
+        )}
+        {cartItems.length >= 1 &&
+          cartItems.map((item) => {
+            return (
+              <Card>
+                <img
+                  src={item.image.data.attributes.formats.thumbnail.url}
+                  alt={item.title}
+                />
+                <CardInfo>
+                  <h3>{item.title}</h3>
+                  <h3>{item.price}</h3>
+                  <Checkout>
+                    <span>Quantity: {item.quantity}</span>
+                    <button>
+                      <AiFillMinusCircle />
+                    </button>
+                    <button>
+                      <AiFillPlusCircle />
+                    </button>
+                  </Checkout>
+                </CardInfo>
+              </Card>
+            );
+          })}
+      </CartStyle>
+    </CartWrapper>
+  );
+}
