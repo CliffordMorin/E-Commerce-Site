bd9ddfa85d7fa9cf0e5a2374a20aad2d52ced3ee
Clifford Morin <cemorin21@gmail.com>
2022-06-25 04:06:15 -0400

    added coupon, new shipping methid and fixed errors with stripe! you can now officially buy things '

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -13,16 +13,29 @@ export default async function handler(req, res) {
         shipping_address_collection: {
           allowed_countries: ["US"],
         },
+        allow_promotion_codes: true,
+        shipping_options: [
+          {
+            shipping_rate: "shr_1LE7faBoahtOp9MLYPgnfRqN",
+          },
+          {
+            shipping_rate: "shr_1LETxKBoahtOp9MLE2fC0y2e",
+          },
+        ],
+
         line_items: req.body.map((item) => {
           return {
             price_data: {
               currency: "usd",
               product_data: {
                 name: item.title,
-
                 images: [item.image.data.attributes.formats.thumbnail.url],
               },
               unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
             },
             quantity: item.quantity,
           };
