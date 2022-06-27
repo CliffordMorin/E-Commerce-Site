55dab6e2f86486e47d4fb962991be41bd6bb34a5
Clifford Morin <cemorin21@gmail.com>
2022-06-27 02:01:34 -0400

    added customer: stripeId to stripe.js to get the user's info if they are logged in

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -1,14 +1,20 @@
 import Stripe from "stripe";
-
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default async function handler(req, res) {
+  const session = await getSession(req, res);
+  const user = session?.user;
+  const stripeId = user["http://localhost:3000/stripe_customer_id"];
+  console.log(stripeId);
+
   if (req.method === "POST") {
     try {
       //CREATE CHECKOUT SESSION from body params
       const session = await stripe.checkout.sessions.create({
         submit_type: "pay",
         mode: "payment",
+        customer: stripeId,
         payment_method_types: ["card"],
         shipping_address_collection: {
           allowed_countries: ["US"],
