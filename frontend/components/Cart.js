874b3daa2ce255472c6ee3ee37af877f6d9a3b6d
Clifford Morin <cemorin21@gmail.com>
2022-07-04 00:38:57 -0400

    added acces control allow origin to header in cart.js

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -41,6 +41,7 @@ export default function Cart() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify(cartItems),
     });
