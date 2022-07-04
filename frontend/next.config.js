c7eb699320d3d91447ff52e13160a3dd2be8ab6f
Clifford Morin <cemorin21@gmail.com>
2022-07-03 23:27:03 -0400

    aadded experimental into next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,12 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-   future: {
-    webpack5: true
-  }
+  future: {
+    webpack5: true,
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
   async redirects() {
     return [{ source: "/canceled", destination: "/", permanent: true }];
   },
