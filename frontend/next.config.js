b94c57388fab8dc99fb3240c74be3d958e1a44ce
Clifford Morin <cemorin21@gmail.com>
2022-07-03 23:14:00 -0400

    added webpack 5 to next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+   future: {
+    webpack5: true
+  }
   async redirects() {
     return [{ source: "/canceled", destination: "/", permanent: true }];
   },
