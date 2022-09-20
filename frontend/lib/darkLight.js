1b9bb6b2f3b05b423f8ebf4affe81aa3aa2ace86
Clifford Morin <cemorin21@gmail.com>
2022-09-20 01:07:26 -0400

    adding dark mode

---
+++ frontend/lib/darkLight.js
@@ -0,0 +1,28 @@
+const darkTheme = {
+  "--background-color": "#000",
+  "--text-color": "#fff",
+  "--text-color-light": "#ccc",
+  "--text-color-dark": "#000",
+  "--text-color-lighter": "#eee",
+  "--text-color-darker": "#333",
+  "--text-color-lightest": "#f5f5f5",
+  "--text-color-darkest": "#222",
+};
+
+const lightTheme = {
+  "--background-color": "#fff",
+  "--text-color": "#000",
+  "--text-color-light": "#ccc",
+  "--text-color-dark": "#000",
+  "--text-color-lighter": "#eee",
+  "--text-color-darker": "#333",
+  "--text-color-lightest": "#f5f5f5",
+  "--text-color-darkest": "#222",
+};
+
+const themes = {
+  dark: darkTheme,
+  light: lightTheme,
+};
+
+export default themes;
